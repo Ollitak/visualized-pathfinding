51a46929f44952fb8f33e30498122593aec00753
Ollitak <olli.takaanttila@gmail.com>
2022-01-28 19:58:23 +0200

    add delay to improve animation

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var ROW_LEN = 30
-var COL_HEIGHT = 30
-var NUMBER_OF_WALLS = 100;
+var ROW_LEN = 80
+var COL_HEIGHT = 40
+var NUMBER_OF_WALLS = 500;
+var SOLVE_SPEED = 0;
 
-
+var found = false;
 var grid = []
 var start = undefined
-var goal = undefined
 
 generate_grid = () => {
   grid = Array(COL_HEIGHT).fill().map(() => Array(ROW_LEN).fill("."));
@@ -23,7 +23,6 @@ generate_grid = () => {
 
   var goal_i = Math.floor(Math.random() * COL_HEIGHT);
   var goal_j = Math.floor(Math.random() * ROW_LEN);
-  goal = [goal_i, goal_j]
   grid[goal_i][goal_j] = "B"
 }
 
@@ -43,15 +42,15 @@ render_grid = () => {
       switch(grid[i][j]){
         case "#":
           new_block.classList.add(...["block", "block-wall"])
-          new_block.appendChild(document.createTextNode("W"))
+          //new_block.appendChild(document.createTextNode("W"))
           break
         case "A":
           new_block.classList.add(...["block", "block-character"]) 
-          new_block.appendChild(document.createTextNode("A"))
+          //new_block.appendChild(document.createTextNode("A"))
           break
         case "B":
           new_block.classList.add(...["block", "block-goal"]) 
-          new_block.appendChild(document.createTextNode("B"))
+          //new_block.appendChild(document.createTextNode("B"))
           break
         case ".":
           new_block.classList.add(...["block", "block-unvisited"])         
@@ -70,7 +69,7 @@ mark_visited = (node, value) => {
 }
 
 
-bfs = () => {
+bfs = async () => {
   var memory = [];
   var visited = Array(COL_HEIGHT).fill().map(() => Array(ROW_LEN).fill(0));
   var shortestPath = Array(COL_HEIGHT).fill().map(() => Array(ROW_LEN).fill(999));
@@ -79,44 +78,55 @@ bfs = () => {
   visited[start[0]][start[1]] = 1
   shortestPath[start[0]][start[1]] = 0;
 
+  const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
   while(memory.length != 0) {
     var current_node = memory.shift()
 
     var next_node_i = current_node[0]+1
     var next_node_j = current_node[1]
     check_neighbor(shortestPath, memory, visited, next_node_i, next_node_j, current_node)
+    await sleep(SOLVE_SPEED)
 
     var next_node_i = current_node[0]-1
     var next_node_j = current_node[1]
     check_neighbor(shortestPath, memory, visited, next_node_i, next_node_j, current_node)
+    await sleep(SOLVE_SPEED)
 
 
     var next_node_i = current_node[0]
     var next_node_j = current_node[1]+1
     check_neighbor(shortestPath, memory, visited, next_node_i, next_node_j, current_node)
+    await sleep(SOLVE_SPEED)
 
 
     var next_node_i = current_node[0]
     var next_node_j = current_node[1]-1
     check_neighbor(shortestPath, memory, visited, next_node_i, next_node_j, current_node)
+    await sleep(SOLVE_SPEED)
+
+    if(found) return;
   }
 };
 
 
 check_neighbor = (shortestPath, memory, visited, next_node_i, next_node_j, current_node) => {
-    var current_node_i = current_node[0]
-    var current_node_j = current_node[1]
-    
-    if (next_node_i >= 0 && next_node_j >= 0 && next_node_j < ROW_LEN && next_node_i < COL_HEIGHT) {
-      if (grid[next_node_i][next_node_j] != "#" && visited[next_node_i][next_node_j] == 0) {
-        visited[next_node_i][next_node_j] = 1;
-        shortestPath[next_node_i][next_node_j] = shortestPath[current_node_i][current_node_j]+1;
-        mark_visited([next_node_i, next_node_j], shortestPath[current_node_i][current_node_j]+1)
-        memory.push([next_node_i, next_node_j])
+  var current_node_i = current_node[0]
+  var current_node_j = current_node[1]
+  
+  if (next_node_i >= 0 && next_node_j >= 0 && next_node_j < ROW_LEN && next_node_i < COL_HEIGHT) {
+    if (grid[next_node_i][next_node_j] != "#" && visited[next_node_i][next_node_j] == 0) {
+      visited[next_node_i][next_node_j] = 1;
+      shortestPath[next_node_i][next_node_j] = shortestPath[current_node_i][current_node_j]+1;
+      mark_visited([next_node_i, next_node_j], shortestPath[current_node_i][current_node_j]+1)
+      memory.push([next_node_i, next_node_j])
+
+      if(grid[next_node_i][next_node_j] == "B") {
+        found = true;
       }
     }
+  }
 }
-
 
 unsolve = () => {
   var main_div = document.getElementById("app")
