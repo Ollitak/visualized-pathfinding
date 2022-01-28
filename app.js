a7f8f705ed2cb2d273451825487c4fc57c201845
Ollitak <olli.takaanttila@gmail.com>
2022-01-28 19:25:28 +0200

    grid is now randomized

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
-const nodes = [
-  ["#","#","#","#","#","#","#","#","#","#"],
-  ["#",".",".",".",".","#",".",".",".","#"],
-  ["#",".",".",".",".",".",".","#",".","#"],
-  ["#",".",".",".",".","B",".",".",".","#"],
-  ["#",".",".","#",".",".",".",".",".","#"],
-  ["#",".",".",".","#",".",".",".",".","#"],
-  ["#","A",".",".",".","#","#",".",".","#"],
-  ["#",".",".",".",".",".",".",".",".","#"],
-  ["#",".","#",".",".",".",".",".",".","#"],
-  ["#","#","#","#","#","#","#","#","#","#"],
-]
+var ROW_LEN = 30
+var COL_HEIGHT = 30
+var NUMBER_OF_WALLS = 100;
 
-var start = [6,1]
-var rowLen = nodes[0].length 
-var colHeight = nodes.length
 
-render_nodes = () => {
+var grid = []
+var start = undefined
+var goal = undefined
+
+generate_grid = () => {
+  grid = Array(COL_HEIGHT).fill().map(() => Array(ROW_LEN).fill("."));
+
+  for (var i = 0; i < NUMBER_OF_WALLS; i++) {
+    var randomized_i = Math.floor(Math.random() * COL_HEIGHT);
+    var randomized_j = Math.floor(Math.random() * ROW_LEN);
+    grid[randomized_i][randomized_j] = "#"
+  }
+
+  var start_i = Math.floor(Math.random() * COL_HEIGHT);
+  var start_j = Math.floor(Math.random() * ROW_LEN);
+  start = [start_i, start_j]
+  grid[start_i][start_j] = "A"
+
+  var goal_i = Math.floor(Math.random() * COL_HEIGHT);
+  var goal_j = Math.floor(Math.random() * ROW_LEN);
+  goal = [goal_i, goal_j]
+  grid[goal_i][goal_j] = "B"
+}
+
+
+render_grid = () => {
   var main_div = document.getElementById("app");
 
-  for(var i = 0; i < nodes.length; i++ ) {
+  for(var i = 0; i < grid.length; i++ ) {
     var new_row = document.createElement("div")
     new_row.className = "container";
     main_div.appendChild(new_row)
 
-    for(var j = 0; j < nodes[i].length; j++){
+    for(var j = 0; j < grid[i].length; j++){
       var new_block = document.createElement("div")
       new_block.setAttribute("id", [i,j])
      
-      switch(nodes[i][j]){
+      switch(grid[i][j]){
         case "#":
           new_block.classList.add(...["block", "block-wall"])
           new_block.appendChild(document.createTextNode("W"))
@@ -59,8 +72,8 @@ mark_visited = (node, value) => {
 
 bfs = () => {
   var memory = [];
-  var visited = Array(colHeight).fill().map(() => Array(rowLen).fill(0));
-  var shortestPath = Array(colHeight).fill().map(() => Array(rowLen).fill(999));
+  var visited = Array(COL_HEIGHT).fill().map(() => Array(ROW_LEN).fill(0));
+  var shortestPath = Array(COL_HEIGHT).fill().map(() => Array(ROW_LEN).fill(999));
 
   memory.push(start);
   visited[start[0]][start[1]] = 1
@@ -91,25 +104,25 @@ bfs = () => {
 
 
 check_neighbor = (shortestPath, memory, visited, next_node_i, next_node_j, current_node) => {
-  var current_node_i = current_node[0]
-  var current_node_j = current_node[1]
-  
-  if (next_node_i >= 0 && next_node_j >= 0 && next_node_j < rowLen && next_node_i < colHeight) {
-    if (nodes[next_node_i][next_node_j] != "#" && visited[next_node_i][next_node_j] == 0) {
-      visited[next_node_i][next_node_j] = 1;
-      shortestPath[next_node_i][next_node_j] = shortestPath[current_node_i][current_node_j]+1;
-      mark_visited([next_node_i, next_node_j], shortestPath[current_node_i][current_node_j]+1)
-      memory.push([next_node_i, next_node_j])
+    var current_node_i = current_node[0]
+    var current_node_j = current_node[1]
+    
+    if (next_node_i >= 0 && next_node_j >= 0 && next_node_j < ROW_LEN && next_node_i < COL_HEIGHT) {
+      if (grid[next_node_i][next_node_j] != "#" && visited[next_node_i][next_node_j] == 0) {
+        visited[next_node_i][next_node_j] = 1;
+        shortestPath[next_node_i][next_node_j] = shortestPath[current_node_i][current_node_j]+1;
+        mark_visited([next_node_i, next_node_j], shortestPath[current_node_i][current_node_j]+1)
+        memory.push([next_node_i, next_node_j])
+      }
     }
-  }
 }
 
 
 unsolve = () => {
   var main_div = document.getElementById("app")
   main_div.innerHTML = ''
-  render_nodes()
+  render_grid()
 }
 
-
-render_nodes()
+generate_grid()
+render_grid()
